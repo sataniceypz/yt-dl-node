0a45d01114143c838d60f6090a7716844ff3c9f3
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-10-21 03:05:11 +0530

    ytdl

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "ytdl",
+  "version": "1.0.0",
+  "description": "simple ytdl",
+  "main": "./eypz.js",
+  "scripts": {
+    "start": "node ./eypz.js"
+  },
+  "keywords": [
+    "api",
+    "ytdl",
+    "Eypz"
+  ],
+  "author": "EYPZ",
+  "license": "GPL-3.0-or-later",
+  "bugs": {
+    "url": "https://github.com/sataniceypz/yt-dl-node/issues"
+  },
+  "homepage": "https://github.com/sataniceypz/yt-dl-node#readme",
+  "dependencies": {
+    "express": "^4.18.2",
+    "api-dylux": "^1.8.5"
+  }
+}
